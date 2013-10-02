088c87bb3fb3706918d9d01fb17557747d5d5de3
Peter Gasston <peter@rehabstudio.com>
2013-10-02 17:40:55 +0100

    Commented out InfluAds calls

--- sidebar.php
+++ sidebar.php
@@ -3,7 +3,7 @@
 			<h2>The Modern Web</h2>
 			<p>My new book is out now from No Starch Press. <a href="http://nostarch.com/modernweb/">Order from the publisher</a> (print copy comes with <b>free</b> eBook) or from <a href="http://www.amazon.co.uk/gp/product/1593274874/ref=as_li_qf_sp_asin_tl?ie=UTF8&camp=1634&creative=6738&creativeASIN=1593274874&linkCode=as2&tag=brokenlinks-21">Amazon.co.uk</a>, <a href="http://www.amazon.com/gp/product/1593274874/ref=as_li_qf_sp_asin_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=1593274874&linkCode=as2&tag=broklink-20">Amazon.com</a>, <a href="http://www.barnesandnoble.com/w/the-modern-web-peter-gasston/1114672512">Barnes &amp; Noble</a> and many more booksellers.</p>
 		</div>
-<div id="influads_block" class="influads_block"></div>
+<!--div id="influads_block" class="influads_block"></div-->
 <?php if ( is_active_sidebar(2) ) : ?>
 	<?php dynamic_sidebar( 'Posts' ); ?>
 <?php endif; ?>

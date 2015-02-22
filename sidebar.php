ca655b7387c9ce03042523e9db69cbe77a244627
Peter Gasston <peter@rehabstudio.com>
2015-02-22 23:16:50 +0000

    Add new book details to sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,7 +1,7 @@
 	<div class="navigation site_meta_container">
 		<div class="book">
-			<h2>The Modern Web</h2>
-			<p>My new book is out now from No Starch Press. <a href="http://nostarch.com/modernweb/">Order from the publisher</a> (print copy comes with <b>free</b> eBook) or from <a href="http://www.amazon.co.uk/gp/product/1593274874/ref=as_li_qf_sp_asin_tl?ie=UTF8&camp=1634&creative=6738&creativeASIN=1593274874&linkCode=as2&tag=brokenlinks-21">Amazon.co.uk</a>, <a href="http://www.amazon.com/gp/product/1593274874/ref=as_li_qf_sp_asin_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=1593274874&linkCode=as2&tag=broklink-20">Amazon.com</a>, <a href="http://www.barnesandnoble.com/w/the-modern-web-peter-gasston/1114672512">Barnes &amp; Noble</a> and many more booksellers.</p>
+			<h2>The Book of CSS3, 2nd Ed.</h2>
+			<p>Fully revised and updated, with two all-new chapters, out now from No Starch Press. <a href="http://www.nostarch.com/css3_2e">Order from the publisher</a> (print copy comes with <b>free</b> eBook) or from <a href="http://www.amazon.co.uk/dp/1593275803/">Amazon.co.uk</a>, <a href="http://www.amazon.com/dp/1593275803/">Amazon.com</a>, and many more booksellers.</p>
 		</div>
 <!--div id="influads_block" class="influads_block"></div-->
 <?php if ( is_active_sidebar(2) ) : ?>
@@ -10,7 +10,7 @@
 		<!--ul class="posts-popular">
 		<li><h3>Popular Posts</h3></li>
 		<?php getPopularPosts(5,7,' &raquo; Broken Links'); ?>
-		</ul-->	
+		</ul-->
 	<hr class="ornament" />
 	<ul>
 	<li class="rss"><a href="<?php bloginfo('rss2_url'); ?>">Subscribe to the RSS Feed</a></li>
